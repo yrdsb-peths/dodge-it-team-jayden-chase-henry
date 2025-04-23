beb39997acde3f97cc244a37ce134486c0c57455
chsCltr <chscltr@gmail.com>
2025-04-23 14:32:49 -0400

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -2,6 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Hero extends Actor
 {
     int cooldown = 0;
+    int position = 1;
     public Hero()
     {
         setImage("images/ppl1.png");
@@ -15,18 +16,20 @@ public class Hero extends Actor
         
         if(Greenfoot.isKeyDown("up")||(Greenfoot.isKeyDown("w")))
         {
-            if (cooldown == 0)
+            if (cooldown == 0&&position > 1)
             {
                 setLocation(getX(), getY()-100);
                 cooldown = 10;
+                position--;
             }
         }
         else if (Greenfoot.isKeyDown("down")||(Greenfoot.isKeyDown("s")))
         {
-            if (cooldown == 0)
+            if (cooldown == 0&&position < 3)
             {
                 setLocation(getX(), getY()+100);
                 cooldown = 10;
+                position++;
             }
         }
     }
