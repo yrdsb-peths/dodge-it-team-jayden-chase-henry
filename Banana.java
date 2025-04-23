c12a22e2f160adf1ba42c452e14cc619bdb979f5
ComplexUsernames <349462549@gapps.yrdsb.ca>
2025-04-23 08:51:51 -0400

    Add banana and skull

---
+++ Banana.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Banana extends Actor
+{
+    public void act()
+    {
+        move(-10);
+        
+        if(getX() <= 0)
+        {
+            resetBanana();
+        }
+        
+        if(isTouching(Banana.class))
+        {
+            Skull skull = new Skull();
+            getWorld().addObject(skull, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void resetBanana()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0)
+        {
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
+    }
+}
