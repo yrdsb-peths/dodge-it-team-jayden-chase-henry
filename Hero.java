e4ee2b9743327b75920052303827a3b32763aa2d
chsCltr <chscltr@gmail.com>
2025-04-23 09:27:04 -0400

    created 3 path movement
    changed hero movement to 3 instead of 2 paths

--- Hero.java
+++ Hero.java
@@ -1,20 +1,33 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Hero extends Actor
 {
+    int cooldown = 0;
     public Hero()
     {
         setImage("images/ppl1.png");
     }
     public void act()
     {
+        if (cooldown > 0)
+        {
+            cooldown --;
+        }
         
         if(Greenfoot.isKeyDown("up")||(Greenfoot.isKeyDown("w")))
         {
-            setLocation(100,100);
+            if (cooldown == 0)
+            {
+                setLocation(getX(), getY()-100);
+                cooldown = 10;
+            }
         }
         else if (Greenfoot.isKeyDown("down")||(Greenfoot.isKeyDown("s")))
         {
-            setLocation(100,300);
+            if (cooldown == 0)
+            {
+                setLocation(getX(), getY()+100);
+                cooldown = 10;
+            }
         }
     }
 }
